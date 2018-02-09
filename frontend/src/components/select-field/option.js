a31150941b66c30833338ae3d1ff31f903e2838c
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-02-09 08:51:07 -0200

    Trying to do a select field

---
+++ frontend/src/components/select-field/option.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Option = ({ value, children }) => (
+  <option value={value}>
+    { children }
+  </option>
+)
+
+Option.propTypes = {
+  value: PropTypes.string,
+  children: PropTypes.string.isRequired
+}
+
+export default Option
