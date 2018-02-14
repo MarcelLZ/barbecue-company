5e46e9b2cc74648aeefe1e645b097732c5bc8ecf
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-02-14 00:22:16 -0200

    Module to authenticate user

---
+++ backend/src/api/auth/controller.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+import { success, notFound, error } from '../../utils/responses'
+import config from '../../../config'
+import encrypt from '../../utils/encrypt'
+import { User } from '../signup'
+
+export const authenticate = ({ body }, res, next) => {
+  const { email, password } = body
+  const encryptedPassword = encrypt(password)
+
+  const generateToken = () =>
+    jwt.sign({ email }, config.secretToken, { expiresIn: '1d' })
+
+  User
+    .findOne({ email, password: encryptedPassword })
+    .then(notFound(res))
+    .then(generateToken)
+    .then(success(res))
+    .catch(error(res))
+}
