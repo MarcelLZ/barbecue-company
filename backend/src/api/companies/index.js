c9c39eb7fd5832a9626dbb7c622a34518e0b3904
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-02-14 02:58:30 -0200

    Applying token validation

--- backend/src/api/companies/index.js
+++ backend/src/api/companies/index.js
@@ -1,15 +1,37 @@
 import { Router } from 'express'
 import { middleware as body } from 'bodymen'
+import token from '../../utils/token-validation'
 import { index, create, addOrder, cancelOrder } from './controller'
 import Company, { orderSchema } from './model'
 
 const app = Router()
 const { name, cnpj } = Company.schema.tree
 
-app.get('/', index)
-app.post('/', body({ name, cnpj }), create)
-app.post('/orders', body({ orders: [orderSchema] }), addOrder)
-app.delete('/:id/order/:orderId', cancelOrder)
+app.get(
+  '/',
+  token({ required: true }),
+  index
+)
+
+app.post(
+  '/',
+  token({ required: true }),
+  body({ name, cnpj }),
+  create
+)
+
+app.post(
+  '/orders',
+  token({ required: true }),
+  body({ orders: [orderSchema] }),
+  addOrder
+)
+
+app.delete(
+  '/:id/order/:orderId',
+  token({ required: true }),
+  cancelOrder
+)
 
 export { Company }
 export default app
--- backend/src/api/orders/index.js
+++ backend/src/api/orders/index.js
@@ -1,8 +1,18 @@
 import { Router } from 'express'
+import token from '../../utils/token-validation'
 import { index } from './controller'
 const app = new Router()
 
-app.get('/', index)
-app.get('/company/:id', index)
+app.get(
+  '/',
+  token({ required: true }),
+  index
+)
+
+app.get(
+  '/company/:id',
+  token({ required: true }),
+  index
+)
 
 export default app
