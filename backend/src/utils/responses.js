7417b808f0435714dc74a070a1b774f7626e11d6
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-02-14 00:21:48 -0200

    Function to encrypt the password to search user and improved notFound method to finish the request

--- backend/src/utils/responses.js
+++ backend/src/utils/responses.js
@@ -1,8 +1,15 @@
 export const success = (response, code = 200) => payload =>
   response.status(code).json(payload)
 
-export const notFound = response => payload =>
-   response.status(404).json(payload)
+export const notFound = response => payload => {
+  if (payload) return payload
+
+  response
+    .status(404)
+    .end()
+
+  return null
+}
 
 export const error = response => error =>
-  response.status(400).json({ message: error.message })
+  response.status(500).json({ message: error.message })
