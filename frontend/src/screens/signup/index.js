420e3d0407a314e94a3a5e10f3a71578cfaf5a70
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-02-16 02:42:02 -0200

    Notifications on signup

--- frontend/src/screens/signup/index.js
+++ frontend/src/screens/signup/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Card, CardText } from 'react-mdl'
 
+import Notifications from 'components/notifications'
 import SignupForm from './form'
 
 import style from './new-account.styl'
 
 const Signup = () => (
   <div className={style.container}>
+    <Notifications />
     <Card shadow={1}>
       <CardText>
         <SignupForm />
