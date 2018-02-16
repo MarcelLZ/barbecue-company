52b8345fdb50043ef3845d603df59a471c1be1b7
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-02-15 22:07:17 -0200

    Validations and actions

--- backend/src/api/users/index.js
+++ backend/src/api/users/index.js
@@ -1,9 +1,24 @@
 import { Router } from 'express'
+import { middleware as body } from 'bodymen'
+import token from '../../utils/token-validation'
+import { create, update } from './controller'
 import User from './model'
 
 const app = new Router()
+const { email, password } = User.schema.tree
 
-app.patch('/me')
+app.post(
+  '/',
+  body({ email, password }),
+  create
+)
+
+app.patch(
+  '/me',
+  token({ required: true }),
+  body({ password }),
+  update
+)
 
 export { User }
 export default app
