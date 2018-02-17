69d6a002afb894eb0d966cd2eeb31cabf5fdfc67
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-02-17 11:51:30 -0200

    <ActiveContent /> tests

--- frontend/src/components/active-content/index.js
+++ frontend/src/components/active-content/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const ActiveContent = ({ active, children }) => (
   <React.Fragment>
     { React.Children.map(children, (element, key) => key === active && element) }
   </React.Fragment>
 )
+
+ActiveContent.propTypes = {
+  active: PropTypes.number,
+  children: PropTypes.node
+}
+
+ActiveContent.defaultProps = {
+  active: 0
+}
 
 export default ActiveContent
